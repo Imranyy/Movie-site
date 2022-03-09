cd8f2b3b26fdb3413af97fabcfe7228163e0b69b
imran matano <89136657+Imranyy@users.noreply.github.com>
2022-03-08 23:16:26 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+//import App from './App';
+import Land from './components/Land';
 
 
 ReactDOM.render(
-    <App />,
+    <Land />,
   document.getElementById('root')
 );
